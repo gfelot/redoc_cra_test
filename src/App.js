5baf28b8ff9c5682660d4221ea19be182357ae06
Gil <gfelot@gmail.com>
2018-08-14 11:34:07 +0200

    Add swagger file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { RedocStandalone } from 'redoc';
 
@@ -7,14 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <RedocStandalone specUrl="http://rebilly.github.io/ReDoc/swagger.yaml" />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <RedocStandalone specUrl="../public/swagger.yml" />
       </div>
     );
   }
